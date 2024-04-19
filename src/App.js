012a79f0a3faff36b2c225293aebdb99a6369a0c
Vinay7995596 <ragichattuvinay7995@gmail.com>
2024-04-19 16:08:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import backgroundImage from './pexels-karolina-grabowska-4397907.jpg'
+import Button from 'react-bootstrap/Button';
 
-function App() {
+const App = () => {
+  const [count, updateCount] = useState(250 * 6);
+  const [running, norunning] = useState(false);
+  const intervalRef = useRef(null)
+
+  useEffect(() =>{
+    if (running) {
+      intervalRef.current = setInterval(() => {
+        updateCount(prevCount => {
+          if (prevCount === 0) {
+            clearInterval(intervalRef.current)
+            return 0
+          }
+          else {
+            return prevCount - 1
+          }
+        })
+      }, 1000)
+    }
+    else {
+      clearInterval(intervalRef.current)
+    }
+},[running])
+
+const formtime = (count) => {
+  const minutes = Math.floor(count / 60)
+  const seconds = count % 60
+  return `${minutes}: ${seconds}`
+}
+
+const timerStarts = () => {
+  norunning(true)
+}
+
+const paueButton = () => {
+  norunning(false)
+}
+
+const resetButton = () => {
+  norunning(false)
+  updateCount(250*6)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='backgroung-main-container'>
+      <div className='conatiner-center'>
+      <div>
+        <h3 className='heading'>Timer</h3>
+      </div>
+      <div className='timer-container'>
+      <p className='number-counting'>{formtime(count)}</p>
+      <button className='btn' onClick={timerStarts}>Start</button>{'  '}
+      <button className='btn' onClick={paueButton}>Pause</button> {'  '}
+      <button className='btn' onClick={resetButton}>Reset</button>
+      </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
